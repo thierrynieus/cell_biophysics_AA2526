006cb43bc6c32b54f81ca74485d49f6da73427e9
Thierry <thierry.ralph.nieus@gmail.com>
2025-12-10 09:59:14 +0100

    Lesson 3 - update 2

--- x86_64/mod_func.cpp
+++ x86_64/mod_func.cpp
@@ -6,20 +6,26 @@ extern int nrn_nobanner_;
 extern "C" {
 #endif
 
+extern void _Grc_sine_reg(void);
 extern void _leak_reg(void);
 extern void _potassium_reg(void);
+extern void _ramp_reg(void);
 extern void _sodium_reg(void);
 
 void modl_reg() {
   if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
     fprintf(stderr, "Additional mechanisms from files\n");
+    fprintf(stderr, " \"Grc_sine.mod\"");
     fprintf(stderr, " \"leak.mod\"");
     fprintf(stderr, " \"potassium.mod\"");
+    fprintf(stderr, " \"ramp.mod\"");
     fprintf(stderr, " \"sodium.mod\"");
     fprintf(stderr, "\n");
   }
+  _Grc_sine_reg();
   _leak_reg();
   _potassium_reg();
+  _ramp_reg();
   _sodium_reg();
 }
 
